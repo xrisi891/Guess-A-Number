cf2caf64479961c1cd6c1d9606a2c45042a83733
xrisi891 <xrisi.89@gmail.com>
2024-03-03 11:17:36 +0200

    Add project file Guess A Number

---
+++ Guess A Number.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random randomNumber = new Random();
+int compNumber=randomNumber.Next(1, 101);
+while (true)
+{
+	Console.WriteLine("Guess a number (1-100)");
+	string inputPlayer=Console.ReadLine();
+	bool isTrue=int.TryParse(inputPlayer, out int playerNumber);
+	if (isTrue )
+	{
+		if (playerNumber == compNumber)
+		{
+			Console.WriteLine("You guessed it!");	break;
+		}
+		else if (playerNumber < compNumber)
+		{
+			Console.WriteLine("Too Low");
+		}
+		else 
+		{
+			Console.WriteLine("Too Hight");
+		}
+        }
+}
